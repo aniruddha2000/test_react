1e433431e5e973baf3eb52eae8e9660b77856507
Aniruddha Basak <codewithaniruddha@gmail.com>
2020-06-30 21:35:33 +0530

    Updates

---
+++ precache-manifest.8b48e707f0e6a71ea146853e38e4900c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aea19477624815013c730989307b99d5",
+    "url": "/test_react/index.html"
+  },
+  {
+    "revision": "8fd7058f3ec0364291e3",
+    "url": "/test_react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9f992639ff35986d2827",
+    "url": "/test_react/static/js/2.73878dfa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test_react/static/js/2.73878dfa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fd7058f3ec0364291e3",
+    "url": "/test_react/static/js/main.eff2a027.chunk.js"
+  },
+  {
+    "revision": "5f851837011c24b7ff1f",
+    "url": "/test_react/static/js/runtime-main.227f5ba9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test_react/static/media/logo.5d5d9eef.svg"
+  }
+]);
